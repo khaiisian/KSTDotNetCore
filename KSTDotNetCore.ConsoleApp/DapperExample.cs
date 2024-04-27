97e64af318deab122149b286f078d9e1d6e339ec
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2024-04-28 00:30:46 +0630

    [Compelete] Sql

---
+++ KSTDotNetCore.ConsoleApp/DapperExample.cs
@@ -0,0 +1,118 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSTDotNetCore.ConsoleApp
+{
+    internal class DapperExample
+    {
+        public void Run()
+        {
+            //Read();
+            //Edit(11);
+            //Edit(15);
+            //Create("title", "author", "content");
+            //Update(15, "Title", "AUTHOR", "CONTENT");
+            Delete(11);
+        }
+
+        public void Read()
+        {
+            IDbConnection db = new SqlConnection (Connectionstrings.sqlConnectionStringBuilder.ConnectionString);
+            List <BlogDto> lst =  db.Query<BlogDto>("Select * from Tbl_Blog").ToList(); 
+
+            foreach (BlogDto item in lst)
+            {
+                Console.WriteLine(item.BlogId);
+                Console.WriteLine(item.BlogTitle);
+                Console.WriteLine(item.BlogAuthor);
+                Console.WriteLine(item.BlogContent);
+                Console.WriteLine("-----------------------------------------------------------");
+            }
+        }
+
+        public void Edit(int id)
+        {
+            IDbConnection db = new SqlConnection(Connectionstrings.sqlConnectionStringBuilder.ConnectionString);
+            var item = db.Query<BlogDto>("Select * from Tbl_Blog where BlogId = @BlogId", new BlogDto { BlogId= id}).FirstOrDefault();
+
+            if (item is null)
+            {
+                Console.WriteLine("No data found");
+                return;
+            }
+
+            Console.WriteLine(item.BlogId);
+            Console.WriteLine(item.BlogTitle);
+            Console.WriteLine(item.BlogAuthor);
+            Console.WriteLine(item.BlogContent);
+            
+        }
+
+        public void Create(string  title, string author, string content)
+        {
+            var item = new BlogDto
+            {
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content
+            };
+
+            string query = @"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor
+           ,@BlogContent)";
+
+            IDbConnection db = new SqlConnection(Connectionstrings.sqlConnectionStringBuilder.ConnectionString);
+            int result = db.Execute(query, item);
+            string message = result > 0 ? "Saving successful." : "Saving failed";
+            Console.WriteLine(message);
+        }
+
+        public void Update (int id, string title, string author, string content)
+        {
+            var item = new BlogDto
+            {
+                BlogId = id,
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content
+            };
+
+            string query = @"UPDATE [dbo].[Tbl_Blog]
+   SET [BlogTitle] = @BlogTitle
+      ,[BlogAuthor] = @BlogAuthor
+      ,[BlogContent] = @BlogContent
+ WHERE BlogId = @BlogId";
+
+            IDbConnection db = new SqlConnection(Connectionstrings.sqlConnectionStringBuilder.ConnectionString);
+            int result = db.Execute(query, item);
+
+            string message = result > 0 ? "Updating successful" : "Updating Failed";
+            Console.WriteLine(message);
+        }
+
+        public void Delete (int id)
+        {
+            var item = new BlogDto
+            {
+                BlogId = id,
+            };
+            string query = @"DELETE FROM [dbo].[Tbl_Blog] WHERE BlogId = @BlogId";
+            IDbConnection db = new SqlConnection(Connectionstrings.sqlConnectionStringBuilder.ConnectionString);
+            int result= db.Execute(query,item);
+
+            string message = result > 0 ? "Deleting Successful" : "Deleting Failed";
+            Console.WriteLine(message);
+        }
+    }
+}
--- KSTDotNetCore.ConsoleApp/Program.cs
+++ KSTDotNetCore.ConsoleApp/Program.cs
@@ -37,12 +37,14 @@ Console.WriteLine("Hello, World!");
 //    Console.WriteLine("...........................................");
 //}
 
-AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
 //adoDotNetExample.Read();
 //adoDotNetExample.Create("title", "author", "content");
 //adoDotNetExample.Update(12, "test title", "test author", "test content");
 //adoDotNetExample.Delete(13);
-adoDotNetExample.Edit(14);  
+//adoDotNetExample.Edit(14);  
+DapperExample dapperExample = new DapperExample();
+dapperExample.Run();
 
 
 
