3db68fc0b74d4ba49a9484b37ff4e8973d190c60
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2024-07-04 00:59:53 +0630

    [Complete]SimplePieChart

---
+++ KSTDotNetCore.MvcChartApp/Controllers/ApexChartController.cs
@@ -0,0 +1,16 @@
+﻿using KSTDotNetCore.MvcChartApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KSTDotNetCore.MvcChartApp.Controllers
+{
+    public class ApexChartController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            PieChartModel model = new PieChartModel();
+            model.Series = new List<int>() { 44, 55, 13, 43, 22 };
+            model.Labels = new List<string>() { "Team A", "Team B", "Team C", "Team D", "Team E" };
+            return View(model);
+        }
+    }
+}
