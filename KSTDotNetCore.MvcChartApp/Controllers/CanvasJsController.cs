657714b48baaf86ec6a9e3480ba81fcf61612285
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2024-08-29 00:55:48 +0630

    CanvasJs

---
+++ KSTDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KSTDotNetCore.MvcChartApp.Controllers
+{
+    public class CanvasJsController : Controller
+    {
+        public IActionResult LineChart()
+        {
+            return View();
+        }
+    }
+}
