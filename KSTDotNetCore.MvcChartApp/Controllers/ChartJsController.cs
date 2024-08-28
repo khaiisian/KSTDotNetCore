f38816b7bb2511856ddd10c56fc777b6bf7bc000
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2024-08-28 11:41:41 +0630

    HighChart

---
+++ KSTDotNetCore.MvcChartApp/Controllers/ChartJsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KSTDotNetCore.MvcChartApp.Controllers
+{
+    public class ChartJsController : Controller
+    {
+        public IActionResult ExampleChart()
+        {
+            return View();
+        }
+
+        public IActionResult InterpolationLineChart()
+        {
+            return View();
+        }
+    }
+}
---
+++ KSTDotNetCore.MvcChartApp/Controllers/HighChartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KSTDotNetCore.MvcChartApp.Controllers
+{
+    public class HighChartController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            return View();
+        }
+    }
+}
